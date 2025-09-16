8787f09a46040906b58ebebe911c9f3048d0eb39
juwon7056 <juwon7056@gmail.com>
2025-09-16 17:23:59 +0900

    finished

---
+++ Week 3/hw.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "calc.h"
+
+int main(void) {
+	int A, P, r, n, t;
+	P = 100;
+	n = 12;
+	t = 40;
+	A = pow(P * (1 + (r / n)), (n * t));
+	printf
+
+	return 0;
+}
+
+
